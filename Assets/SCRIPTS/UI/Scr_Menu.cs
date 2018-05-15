a807b4c1120a46be806c314fc007b371aaa3d424
Clekio <alvaro.ortuno.m@gmail.com>
2018-05-15 16:13:11 +0200

    Primera Iteración

---
+++ Assets/SCRIPTS/UI/Scr_Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scr_Menu : MonoBehaviour {
+
+
+    CanvasGroup fadeGroup;
+    float fadeSpeed = 0.5f;
+    
+    
+    
+    // Use this for initialization
+	void Start () {
+
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        fadeGroup.alpha = 1;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeSpeed;
+
+	}
+}
